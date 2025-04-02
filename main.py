647c0ab680c1e94754df48ff7697d577e4f6fd72
Mikhail <gubm04@mail.ru>
2025-04-02 22:01:22 +0500

    fix method on routes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.route('/check_connection', methods=['GET', 'POST'])(check_connection)
-app.route('/get_user_data', methods=['POST'])(get_user_data)
+app.route('/get_user_data', methods=['POST', 'GET'])(get_user_data)
 app.route('/get_schedule', methods=['POST'])(get_schedule)
 app.route('/start_session', methods=['POST'])(start_session)
 app.route('/check_session', methods=['POST'])(check_session)
