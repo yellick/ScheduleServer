5aabc21e4db659bbd6d86e38e49c08e586e1eadb
Mikhail <gubm04@mail.ru>
2025-04-02 22:01:36 +0500

    fix method on routes

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ CORS(app)
 
 app.route('/check_connection', methods=['GET', 'POST'])(check_connection)
 app.route('/get_user_data', methods=['POST', 'GET'])(get_user_data)
-app.route('/get_schedule', methods=['POST'])(get_schedule)
-app.route('/start_session', methods=['POST'])(start_session)
-app.route('/check_session', methods=['POST'])(check_session)
+app.route('/get_schedule', methods=['POST', 'GET'])(get_schedule)
+app.route('/start_session', methods=['POST', 'GET'])(start_session)
+app.route('/check_session', methods=['POST', 'GET'])(check_session)
 
 if __name__ == '__main__':
     host = '0.0.0.0'
